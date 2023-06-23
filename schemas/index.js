09979e7f7397e4d9b98e7c1778f537e4e2a3d86b
danielvorn <danielvorn@gmail.com>
2023-06-23 14:06:29 +0700

    adding press

--- schemas/index.js
+++ schemas/index.js
@@ -3,5 +3,6 @@ import category from './category'
 import post from './post'
 import author from './author'
 import article from './article'
+import press from './press'
 
-export const schemaTypes = [post, author, category, blockContent, article]
+export const schemaTypes = [post, author, category, blockContent, article, press]
